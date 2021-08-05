f2c12d29e589d40852f627f5b9a1a28b83bf6ded
Nino <anthony@delcey.fr>
2021-08-05 15:11:20 +0200

    Initial commit

---
+++ app/src/main/java/com/openclassrooms/netapp/controllers/fragments/MainFragment.java
@@ -0,0 +1,135 @@
+package com.openclassrooms.netapp.controllers.fragments;
+
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.openclassrooms.netapp.models.GithubUser;
+import com.openclassrooms.netapp.models.GithubUserInfo;
+import com.openclassrooms.netapp.R;
+import com.openclassrooms.netapp.utils.GithubStreams;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+    // FOR DESIGN
+    @BindView(R.id.fragment_main_textview) TextView textView;
+
+    //FOR DATA
+    private Disposable disposable;
+
+    public MainFragment() { }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        this.disposeWhenDestroy();
+    }
+
+    // -----------------
+    // ACTIONS
+    // -----------------
+
+    @OnClick(R.id.fragment_main_button)
+    public void submit(View view) {
+        // 2 - Call the stream
+        this.executeSecondHttpRequestWithRetrofit();
+    }
+
+    // -------------------
+    // HTTP (RxJAVA)
+    // -------------------
+
+    // 1 - Execute our Stream
+    private void executeHttpRequestWithRetrofit(){
+        this.updateUIWhenStartingHTTPRequest();
+        this.disposable = GithubStreams.streamFetchUserFollowing("JakeWharton").subscribeWith(new DisposableObserver<List<GithubUser>>() {
+            @Override
+            public void onNext(List<GithubUser> users) {
+                Log.e("TAG","On Next");
+                updateUIWithListOfUsers(users);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e("TAG","On Error"+Log.getStackTraceString(e));
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e("TAG","On Complete !!");
+            }
+        });
+    }
+
+    private void executeSecondHttpRequestWithRetrofit(){
+        this.updateUIWhenStartingHTTPRequest();
+        this.disposable = GithubStreams.streamFetchUserFollowingAndFetchFirstUserInfos("JakeWharton").subscribeWith(new DisposableObserver<GithubUserInfo>() {
+            @Override
+            public void onNext(GithubUserInfo users) {
+                Log.e("TAG","On Next");
+                updateUIWithUserInfo(users);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e("TAG","On Error"+Log.getStackTraceString(e));
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e("TAG","On Complete !!");
+            }
+        });
+    }
+
+    private void disposeWhenDestroy(){
+        if (this.disposable != null && !this.disposable.isDisposed()) this.disposable.dispose();
+    }
+
+    // -------------------
+    // UPDATE UI
+    // -------------------
+
+    private void updateUIWhenStartingHTTPRequest(){
+        this.textView.setText("Downloading...");
+    }
+
+    private void updateUIWhenStopingHTTPRequest(String response){
+        this.textView.setText(response);
+    }
+
+    private void updateUIWithListOfUsers(List<GithubUser> users){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (GithubUser user : users){
+            stringBuilder.append("-"+user.getLogin()+"\n");
+        }
+        updateUIWhenStopingHTTPRequest(stringBuilder.toString());
+    }
+
+    private void updateUIWithUserInfo(GithubUserInfo userInfo){
+        updateUIWhenStopingHTTPRequest("The first Following of Jake Wharthon is "+userInfo.getName()+" with "+userInfo.getFollowers()+" followers.");
+    }
+}
